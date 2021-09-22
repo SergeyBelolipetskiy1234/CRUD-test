8ee93eb8594556bd32c00a2db947e28eb735fac4
SergeyBelolipetskiy1234 <89917779+SergeyBelolipetskiy1234@users.noreply.github.com>
2021-09-22 16:10:44 +0300

    22.09

--- src/main/java/ru/belolipetsckiy/dao/UserDao.java
+++ src/main/java/ru/belolipetsckiy/dao/UserDao.java
@@ -1,47 +1,13 @@
 package ru.belolipetsckiy.dao;
 
-import org.springframework.stereotype.Component;
-import ru.belolipetsckiy.models.Person;
 import ru.belolipetsckiy.models.User;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Component
-public class UserDao {
-    private static int PEOPLE_COUNT;
-    private List<User> users;
-
-    {
-        users = new ArrayList<>();
-
-        users.add(new User(++PEOPLE_COUNT, "Tom", "Abrikosov", 20, "mail@mail.ru"));
-        users.add(new User(++PEOPLE_COUNT, "Jerry", "Alaverdov",20, "mail2@mail.ru"));
-        users.add(new User(++PEOPLE_COUNT, "Dug", "Donald",20, "mail3@mail.ru"));
-        users.add(new User(++PEOPLE_COUNT, "Boris", "Britva",20, "mail4@mail.ru"));
-    }
-    public List<User> index() {
-        return users;
-    }
-
-    public User show(int id) {
-        return users.stream().filter(user-> user.getId() == id).findAny().orElse(null);
-    }
-
-    public void save(User user) {
-        user.setId(++PEOPLE_COUNT);
-        users.add(user);
-    }
-
-    public void update(int id, User updateUser) {
-        User userToBeUpdated= show(id);
-        userToBeUpdated.setName(updateUser.getName());
-        userToBeUpdated.setSurname(updateUser.getSurname());
-        userToBeUpdated.setAge(updateUser.getAge());
-        userToBeUpdated.setEmail(updateUser.getEmail());
-    }
-
-    public void delete(int id) {
-        users.removeIf(u -> u.getId() == id);
-    }
+public interface UserDao {
+    public List<User> index();
+    public User show(int id);
+    public void save(User user);
+    public void update(int id, User updateUser);
+    public void delete(int id);
 }
--- src/main/java/ru/belolipetsckiy/models/User.java
+++ src/main/java/ru/belolipetsckiy/models/User.java
@@ -1,21 +1,29 @@
 package ru.belolipetsckiy.models;
 
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
-
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+    @Column
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
+    @Column
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String surname;
+    @Column
     @Min(value = 0, message = "Age should be greater then 0")
     private int age;
+    @Column
     @NotEmpty(message = "Email should not be empty")
     @Email(message = "Email should be valid")
     private String email;
